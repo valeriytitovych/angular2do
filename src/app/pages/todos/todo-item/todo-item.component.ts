99be1cdec59d190fd64bb113723b34c57e019e8e
Valeriy Titovych <valeriy.titovych@gmail/com>
2020-12-13 22:22:49 +0200

    added todo-item & input

--- src/app/pages/todos/todo-item/todo-item.component.ts
+++ src/app/pages/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
-
+  @Input() todo: [];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
