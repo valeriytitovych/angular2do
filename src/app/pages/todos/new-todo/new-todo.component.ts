32ce55505e3e31ebe7523937d471962f1dcb88d8
Valeriy Titovych <valeriy.titovych@gmail/com>
2020-12-14 22:40:49 +0200

    added form validation

--- src/app/pages/todos/new-todo/new-todo.component.ts
+++ src/app/pages/todos/new-todo/new-todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-todo',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-todo.component.scss']
 })
 export class NewTodoComponent implements OnInit {
+  @ViewChild('newTodoForm') newTodoForm: FormGroup;
+
+  title: string;
+  description: string;
+  isSubmit = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    this.isSubmit = true;
+
+    if (this.newTodoForm.invalid) {
+      return
+    }
+
+    this.isSubmit = false;
+    console.log(
+      this.newTodoForm.value
+    );
+
+    this.newTodoForm.reset();
+  }
 }
